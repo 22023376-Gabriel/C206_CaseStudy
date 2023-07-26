8318f82c141705488c03e258df9b95fbc4ab09f5
22023376 <@22023376@myrp.edu.sg>
2023-07-26 16:28:31 +0800

    Menu for the system

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -2,7 +2,25 @@
 public class C206_CaseStudy {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int option = 0;
+		
+		while (option != 2) {
+			menu();
+			option = Helper.readInt("Enter an option > ");
+
+			if (option == 1) {
+				
+			}
+		}
+		
+	}
+	public static void menu() {
+		Helper.line(80, "-");
+		System.out.println("TUITION MANAGEMENT MENU");
+		Helper.line(80, "-");		
+		System.out.println("1. User Management");
+		System.out.println("2. Quit");
+		Helper.line(80, "-");
 
 	}
 
