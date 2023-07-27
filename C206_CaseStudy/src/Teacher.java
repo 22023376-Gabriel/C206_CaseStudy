d80f38801e2c44b2420f828dd70916f0e336abf1
22023376 <22023376@LAPTOP-PEI2PJ81>
2023-07-27 16:31:17 +0800

    assignedCourses arrayList in Teacher class

--- C206_CaseStudy/src/Teacher.java
+++ C206_CaseStudy/src/Teacher.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 
 public class Teacher extends User{
 	private int teacherId;
 	private String userRole = "Teacher";
+	private ArrayList<Course> assignedCourses = new ArrayList<Course>();
 	
 	public Teacher(String name,int teacherId, String password,String userRole, String email, String mobileNumber) {
 		super(name, password, email, mobileNumber);
