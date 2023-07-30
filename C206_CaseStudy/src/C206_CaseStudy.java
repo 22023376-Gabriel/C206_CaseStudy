9914432ea360f03ceb87c146bb8efd280a590de9
22019418 <22019418@myrp.edu.sg>
2023-07-30 21:18:22 +0800

    Added viewAllCourses and Course arrayList

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -5,13 +5,14 @@ public class C206_CaseStudy {
 	public static void main(String[] args) {
 		ArrayList<Student> studentList = new ArrayList<Student>();
 		ArrayList<User> userList = new ArrayList<User>();
+		ArrayList<Course> courseList = new ArrayList<Course>();
 		
 		studentList.add(new Student(1, "Jack Park"));
 		studentList.add(new Student(2, "Tom Roger", 500.00, "25/09/23"));
 		
 		int option = 0;
 		
-		while (option != 3) { //Modify the quit number whenever you want
+		while (option != 4) { //Modify the quit number whenever you want
 			menu();
 			option = Helper.readInt("Enter an option > ");
 
@@ -22,6 +23,9 @@ public class C206_CaseStudy {
 				viewAllStudents(studentList);
 				
 			}
+			else if (option == 3) {
+				viewAllCourses(courseList);
+			}
 		}
 		
 	}
@@ -31,7 +35,8 @@ public class C206_CaseStudy {
 		Helper.line(80, "-");		
 		System.out.println("1. User Management");
 		System.out.println("2. Display All Students");
-		System.out.println("3. Quit");
+		System.out.println("3. Display All Course");
+		System.out.println("4. Quit");
 		Helper.line(80, "-");
 
 	}
@@ -43,5 +48,12 @@ public class C206_CaseStudy {
 		}
 		System.out.println(output);
 	}
-
+	public static void viewAllCourses(ArrayList<Course> courseList) {
+		String output = "";
+		output += String.format("%-5s %-15s %-10s", "ID", "Name", "Course Fee");
+		for (int i = 0; i < courseList.size(); i++) {
+			output += String.format("%-5d %-15s %-10.2f\n", courseList.get(i).getCourseID(), courseList.get(i).getCourseName(), courseList.get(i).getCourseFee());
+		}
+		System.out.println(output);
+	}
 }
