3cb87292f5f258418d3faf7c8e9b7ecd6e7c7557
gamer <gamer@LAPTOP-OIQJSEHJ>
2023-07-30 20:21:12 +0800

    Added viewAllStudents and edited Student Class

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -6,13 +6,20 @@ public class C206_CaseStudy {
 		ArrayList<Student> studentList = new ArrayList<Student>();
 		ArrayList<User> userList = new ArrayList<User>();
 		
+		studentList.add(new Student(1, "Jack Park"));
+		studentList.add(new Student(2, "Tom Roger", 500.00, "25/09/23"));
+		
 		int option = 0;
 		
-		while (option != 2) { //Modify the quit number whenever you want
+		while (option != 3) { //Modify the quit number whenever you want
 			menu();
 			option = Helper.readInt("Enter an option > ");
 
 			if (option == 1) {
+				
+			}
+			else if (option == 2) {
+				viewAllStudents(studentList);
 				
 			}
 		}
@@ -23,9 +30,18 @@ public class C206_CaseStudy {
 		System.out.println("TUITION MANAGEMENT MENU");
 		Helper.line(80, "-");		
 		System.out.println("1. User Management");
-		System.out.println("2. Quit");
+		System.out.println("2. Display All Students");
+		System.out.println("3. Quit");
 		Helper.line(80, "-");
 
+	}
+	public static void viewAllStudents(ArrayList<Student> studentList) {
+		String output = "";
+		output += String.format("%-5s %-15s %-15s %-10s" , "ID", "Name", "Amount Fee", "Due Date");
+		for (int i = 0; i< studentList.size(); i++) {
+			output += String.format("\n%-5d %-15s %-15.2f %-10s", studentList.get(i).getStudentID(), studentList.get(i).getStudentName(), studentList.get(i).getAmountfee(), studentList.get(i).getDueDate());
+		}
+		System.out.println(output);
 	}
 
 }
--- C206_CaseStudy/src/Student.java
+++ C206_CaseStudy/src/Student.java
@@ -2,22 +2,31 @@
 public class Student {
 	private int studentID;
 	private String studentName;
-	private int amountfee;
+	private double amountfee;
 	private String dueDate;
 	
-	public Student(int studentID, String studentName, int amountfee, String dueDate) {
+	public Student(int studentID, String studentName, double amountfee, String dueDate) {
 		this.studentID = studentID;
 		this.studentName = studentName;
 		this.amountfee = amountfee;
 		this.dueDate = dueDate;
 	}
+	public Student(int studentID, String studentName) {
+		this.studentID = studentID;
+		this.studentName = studentName;
+		this.amountfee =0;
+		this.dueDate ="";
+	}
+	
+	
+	
 	public int getStudentID() {
 		return studentID;
 	}
 	public String getStudentName() {
 		return studentName;
 	}
-	public int getAmountfee() {
+	public double getAmountfee() {
 		return amountfee;
 		
 	}
