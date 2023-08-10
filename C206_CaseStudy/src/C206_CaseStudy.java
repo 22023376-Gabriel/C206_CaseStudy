84502f00dd1e4995f7019186b9c24018db572a2f
gamer <gamer@10.175.5.125>
2023-08-10 10:23:36 +0800

    Some changes to student methods

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -40,7 +40,7 @@ public class C206_CaseStudy {
                     }
                 }
             }
-			else if (option == 2) {
+			else if (option == 2) { //Louis
 				int studentOption = 0;
 				while (studentOption != 4) {
 					studentMenu();
--- C206_CaseStudy/src/Fees.java
+++ C206_CaseStudy/src/Fees.java
@@ -4,11 +4,19 @@ public class Fees {
 	private String feeName;
 	private double feeAmount;
 	private String dueDate;
+	private int feeId;
 	
-	public Fees(String feeName, double feeAmount, String dueDate) {
+	public Fees(String feeName, double feeAmount, String dueDate, int feeId) {
 		this.feeName = feeName;
 		this.feeAmount = feeAmount;
 		this.dueDate = dueDate;
+		this.feeId = feeId;
+	}
+	public int getFeeId() {
+		return feeId;
+	}
+	public void setFeeId(int feeId) {
+		this.feeId = feeId;
 	}
 	public String getFeeName() {
 		return feeName;
