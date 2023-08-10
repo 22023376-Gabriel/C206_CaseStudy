ef8e693ff18b917f9c1c614f7de3181e24945f80
22019418 <22019418@myrp.edu.sg>
2023-08-10 10:00:11 +0800

    hi

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -164,6 +164,7 @@ public void testRemoveStudent() {
 	
 	
 }
+//hi
 @Test
 public void testViewAllCourses() {
     assertNotNull("Test if there is a valid course arraylist to retrieve items from", courseList);
